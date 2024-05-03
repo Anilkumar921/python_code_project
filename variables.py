7e0548726c6c564c5687f32303f459a81c298595
Anilkumar921 <168803289+Anilkumar921@users.noreply.github.com>
2024-05-03 22:04:20 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+# string
+name = "Anil"
+print(type(name))
+# integer
+roll_number = 12
+print(type(roll_number))
+# floating number
+percentage = 53.4
+print(type(percentage))
+# boolean
+is_student = True 
+print(type(is_student))
+print(name,roll_number,is_student,percentage)
+'''print(name,roll_number,percentage,is_student)
+percentage = 67
+print(name,roll_number,percentage,is_student)
+print("hello word")'''
+#print(" my name is " + name + " and my roll number is " ,roll_number)
+#print(expression)
+#print("my percentage has changed to", percentage-1.0)
+#print with separator
+#print(name,roll_number,percentage,is_student, sep="-")
+#sep = example
+x = 1 
+y = 2 
+z = 3
+print(x,y,z, sep="->")
+# then run py. file 
+
